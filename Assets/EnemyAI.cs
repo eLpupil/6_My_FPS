d543d256d760af2cf17a081e9142af22f7644bf7
JX <jxie1995@gmail.com>
2020-05-03 01:20:49 -0700

    3. OnDrawGizmoSelected for enemy chase  range Enemy stop and attack within certain distance Enemy will chase once provoked Added gun asset and crosshairs

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -5,19 +5,65 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
     NavMeshAgent navMeshAgent;
+
+    bool isProvoked = false;
+    float distanceToTarget;
 
     // Start is called before the first frame update
     void Start()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent = GetComponent<NavMeshAgent>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        else
+        {
+            AttackTarget();
+        }
+    }
+
+
+    private void AttackTarget()
+    {
+        print("attacking target");
+    }
+
+    private void ChaseTarget()
+    {
         navMeshAgent.SetDestination(target.position);
+    }
+
+
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 1, 0, 0.75F);
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
