f35da01e3e95ad3c4d69443c9cf74ff8530fe796
JX <jxie1995@gmail.com>
2020-05-04 17:30:26 -0700

    4. RayCast for shooting, Enemy HP script Decrease enemy HP upon hit

---
+++ Assets/Weapons.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float maxRange = 100f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        bool rayHit = Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, maxRange);
+        if (rayHit)
+        {
+            print(hit.transform.name);
+        }
+    }
+}
